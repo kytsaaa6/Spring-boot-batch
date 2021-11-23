58eb470beed6b0da92814b8b83f79150877f121f
kytsaaa6 <kytsaaa6@naver.com>
2021-11-23 23:30:29 +0900

    Initial commit

---
+++ src/test/java/study/kym/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kym.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
