5abcfc809b07e774c38df18f8c70f0fa0891be5f
kytsaaa6 <kytsaaa6@naver.com>
2021-12-09 21:14:13 +0900

    JobLauncher 동기, 비동기 실행 테스트

--- src/main/java/study/kym/batch/JobConfiguration.java
+++ src/main/java/study/kym/batch/JobConfiguration.java
@@ -10,27 +10,18 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * JobRepositroy Test
- * - 배치 작업 중의 정보를 저장하는 저장소 역할
- * - JobExecutionListener 구현하여 Job 실행 정보 가져오기
- * - JobRepository 커스터마이징
- */
-
 @Configuration
 @RequiredArgsConstructor
 public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final JobRepositoryListener jobRepositoryListener;
 
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
-                .listener(jobRepositoryListener)
                 .build();
     }
 
@@ -41,6 +32,7 @@ public class JobConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("step1 was executed");
+                        Thread.sleep(3000);
                         return RepeatStatus.FINISHED;
                     }
                 })
@@ -50,7 +42,13 @@ public class JobConfiguration {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(((stepContribution, chunkContext) -> null))
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step2 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
                 .build();
     }
 
---
+++ src/main/java/study/kym/batch/Member.java
@@ -0,0 +1,9 @@
+package study.kym.batch;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    String id;
+}
